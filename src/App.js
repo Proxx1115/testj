9f9dc262421f48aa8703a7fe7f0056dccde07985
Bayarjargal <79000915+Proxx1115@users.noreply.github.com>
2023-03-08 12:22:03 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import * as React from "react";
+// import { useNavigate } from "react-router-dom";
+
 import LoginForm from "./Login";
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 
 function App() {
+  // const navigate = useNavigate();
+
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
-      <LoginForm />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
